56d380ff69f764147829e9b90144a36d7058223d
David Rice <me@davidjrice.co.uk>
2013-01-15 23:49:02 +0000

    Update and fix specs

--- spec/validator_spec.rb
+++ spec/validator_spec.rb
@@ -3,69 +3,57 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Validator do
 
+  def fixture(file)
+    path = "#{File.dirname(__FILE__)}/fixtures"
+    file = File.open("#{path}/#{file}.json").read
+
+    parser = Yajl::Parser.new
+    parser.parse(file)
+  end
 
   describe "Validator.nu, get -ing" do
 
    it "should receive a no message result" do
-      fixture = File.open("#{File.dirname(__FILE__)}/fixtures/no-message.json").read
-
       Validator.nu(
       URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/no-message.html")
-      ).should == fixture
+      ).should == fixture('no-message')
     end
 
     # http://hsivonen.iki.fi/test/moz/messages-types/info.svg
     it "should receive an info result" do
-      fixture = File.open("#{File.dirname(__FILE__)}/fixtures/info.json").read
-
-
       Validator.nu(
       URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/info.svg")
-      ).should == fixture
+      ).should == fixture('info')
     end
 
     it "should receive a warning result" do
-      fixture = File.open("#{File.dirname(__FILE__)}/fixtures/warning.json").read
-
-
       Validator.nu(
       URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/warning.html")
-      ).should == fixture
+      ).should == fixture('warning')
     end
 
     it "should receive a non-document result" do
-      fixture = File.open("#{File.dirname(__FILE__)}/fixtures/non-document-error.json").read
-
-
       Validator.nu(
       URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/404.html")
-      ).should == fixture
+      ).should == fixture('non-document-error')
     end
 
     it "should receive a precise error result" do
-      fixture = File.open("#{File.dirname(__FILE__)}/fixtures/precise-error.json").read
-
       Validator.nu(
       URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/precise-error.html")
-      ).should == fixture
+      ).should == fixture('precise-error')
     end
 
     it "should receive a range error result" do
-      fixture = File.open("#{File.dirname(__FILE__)}/fixtures/range-error.json").read
       Validator.nu(
       URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/range-error.html")
-      ).should == fixture
+      ).should == fixture('range-error')
     end
 
     it "should receive a fatal error result" do
-      # fixture = File.open("#{File.dirname(__FILE__)}/fixtures/fatal-error.json").read
-      file = File.new("#{File.dirname(__FILE__)}/fixtures/fatal-error.json", 'r')
-      parser = Yajl::Parser.new
-      fixture = parser.parse(file)
-      
       Validator.nu(
         URI.parse("http://hsivonen.iki.fi/test/moz/messages-types/fatal.xhtml")
-      ).should == fixture
+      ).should == fixture('fatal-error')
     end
 
   end
@@ -76,41 +64,45 @@ describe Validator do
       fixture = File.open("#{File.dirname(__FILE__)}/fixtures/no-message-post.json").read
       html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/no-message.html").read
 
-      Validator.nu(html_fixture).should == fixture
+      Validator.nu(html_fixture).force_encoding("UTF-8").should == fixture
     end
 
     it "should receive an info result" do
       fixture = File.open("#{File.dirname(__FILE__)}/fixtures/info-post.json").read
       html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/info.svg").read
 
-      Validator.nu(html_fixture, :content_type => 'image/svg+xml').should == fixture
+      Validator.nu(html_fixture, {
+        :content_type => 'image/svg+xml'
+      }).force_encoding("UTF-8").should == fixture
     end
 
     it "should receive a warning result" do
       fixture = File.open("#{File.dirname(__FILE__)}/fixtures/warning-post.json").read
       html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/warning.html").read
 
-      Validator.nu(html_fixture).should == fixture
+      Validator.nu(html_fixture).force_encoding("UTF-8").should == fixture
     end
 
     it "should receive a precise error result" do
       fixture = File.open("#{File.dirname(__FILE__)}/fixtures/precise-error-post.json").read
       html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/precise-error.html").read
 
-      Validator.nu(html_fixture).should == fixture
+      Validator.nu(html_fixture).force_encoding("UTF-8").should == fixture
     end
 
     it "should receive a range error result" do
       fixture = File.open("#{File.dirname(__FILE__)}/fixtures/range-error-post.json").read
       html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/range-error.html").read
 
-      Validator.nu(html_fixture).should == fixture
+      Validator.nu(html_fixture).force_encoding("UTF-8").should == fixture
     end
 
     it "should receive a fatal error result" do
       fixture = File.open("#{File.dirname(__FILE__)}/fixtures/fatal-error-post.json").read
       html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/fatal-error.html").read
-      Validator.nu( html_fixture, :content_type => 'application/xhtml+xml').should == fixture
+      Validator.nu( html_fixture, {
+        :content_type => 'application/xhtml+xml'
+      }).force_encoding("UTF-8").should == fixture
     end
 
   end
@@ -119,7 +111,9 @@ describe Validator do
     fixture = File.open("#{File.dirname(__FILE__)}/fixtures/info-post.json").read
     html_fixture = File.open("#{File.dirname(__FILE__)}/fixtures/info.svg").read
 
-    Validator.nu(html_fixture, :content_type => 'image/svg+xml', :gzip => true).should == fixture
+    Validator.nu(html_fixture, {
+      :content_type => 'image/svg+xml', :gzip => true
+    }).force_encoding("UTF-8").should == fixture
 
   end
 
